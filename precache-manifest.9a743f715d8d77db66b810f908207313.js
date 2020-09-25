95fcd51415f60d3baadd0add3bad7556c509ac79
code0-st <chumarenko_kir@mail.ru>
2020-09-25 16:55:04 +0300

    Updates

---
+++ precache-manifest.9a743f715d8d77db66b810f908207313.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b0ccfac8c81f9537c0602cb425c6d8e5",
+    "url": "/react-app/index.html"
+  },
+  {
+    "revision": "37072eae96ff841e1f05",
+    "url": "/react-app/static/css/3.05c132d8.chunk.css"
+  },
+  {
+    "revision": "0645c5c8b2443ca9fd59",
+    "url": "/react-app/static/css/4.4a7703e5.chunk.css"
+  },
+  {
+    "revision": "00add224445eef8b6636",
+    "url": "/react-app/static/css/main.560169af.chunk.css"
+  },
+  {
+    "revision": "42ab6af98a3b71d3d0a2",
+    "url": "/react-app/static/js/2.02d2f030.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/react-app/static/js/2.02d2f030.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "37072eae96ff841e1f05",
+    "url": "/react-app/static/js/3.2e28b4d6.chunk.js"
+  },
+  {
+    "revision": "0645c5c8b2443ca9fd59",
+    "url": "/react-app/static/js/4.9463d9c6.chunk.js"
+  },
+  {
+    "revision": "00add224445eef8b6636",
+    "url": "/react-app/static/js/main.e1b139d0.chunk.js"
+  },
+  {
+    "revision": "a2ed5ff07cd9dbcbe186",
+    "url": "/react-app/static/js/runtime-main.0efc6056.js"
+  },
+  {
+    "revision": "f8e835d51ba767f875584a187b2c0b85",
+    "url": "/react-app/static/media/Login_picture.f8e835d5.png"
+  },
+  {
+    "revision": "297747f42d60899a476571b8bf1584a2",
+    "url": "/react-app/static/media/cross.297747f4.svg"
+  },
+  {
+    "revision": "a292ab29b895adefc406b30ac1e36da8",
+    "url": "/react-app/static/media/error.a292ab29.svg"
+  },
+  {
+    "revision": "900ec5a6df455adb47083844048c0a97",
+    "url": "/react-app/static/media/preloader.900ec5a6.svg"
+  },
+  {
+    "revision": "25e5d7a66c58754e19e3ff8943a0d8fc",
+    "url": "/react-app/static/media/prevNextArrow.25e5d7a6.svg"
+  }
+]);
